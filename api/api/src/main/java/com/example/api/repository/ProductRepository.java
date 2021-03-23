92488219bac60301ccbb8c12892a8c0811706b25
User <User@DESKTOP-EQEQ9E1>
2021-03-23 03:10:21 +0200

    v1

---
+++ api/api/src/main/java/com/example/api/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.api.entity.Product;
+
+
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
